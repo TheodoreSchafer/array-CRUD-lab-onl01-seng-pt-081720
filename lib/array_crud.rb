03d740f61ec57d868924c2983bfecae29878f1f9
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-21 16:17:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["1", "2" , "3", "4"]
+  array = ["wow", "I", "am", "really", "learning"]
   element = "arrays!"
   array.push(element)
 end
